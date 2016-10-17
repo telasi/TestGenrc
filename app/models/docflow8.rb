b1773860e601dcd1306ec27e9540830fd7763fa6
T <opit@gol.ge>
2016-10-17 15:43:14 +0400

    docflow 8 type model

---
+++ app/models/docflow8.rb
@@ -0,0 +1,20 @@
+# -*- encoding : utf-8 -*-
+class Docflow8 < ActiveRecord::Base
+	self.table_name = 'docflow8'
+
+  CASE_ID = 8
+  
+  def to_hash
+  	case self.stage
+      when 1
+        fields = [:letter_number, :abonent_number, :abonent,
+                  :abonent_address, :consumer_category, :attach_8_1]
+      when 2
+        fields = [:attach_8_2]
+      when 3
+        fields = [:attach_8_3]
+    end
+    self.serializable_hash(only: fields)
+  end
+
+end
