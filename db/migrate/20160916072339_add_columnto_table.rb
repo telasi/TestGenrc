cf691ca8dbda113a00f073495bd36d1e318dfd35
bacho <bachosh2@gmail.com>
2016-10-24 17:45:52 +0400

    delete migrations

--- db/migrate/20160916072339_add_columnto_table.rb
+++
@@ -1,6 +0,0 @@
-class AddColumntoTable < ActiveRecord::Migration
-  def change
-  	add_column :cr_outages, :disabling_area, :binary
-    add_column :pl_outages, :disabling_area, :binary    
-  end
-end
--- db/migrate/20160919110516_add_responce_case_id.rb
+++ db/migrate/20160919110516_add_responce_case_id.rb
@@ -1,10 +1,7 @@
 class AddResponceCaseId < ActiveRecord::Migration
   def change
 
-  	add_column :cr_outages, :response_id, :integer
-    add_column :pl_outages, :response_id, :integer    
-
-    rename_column :docflow, :case_id, :response_id
+   rename_column :docflow, :case_id, :response_id
 
   end
 end
