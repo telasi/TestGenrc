e16caf2134851d489868fd6282d508c1bbcc23b9
T <opit@gol.ge>
2016-11-18 17:22:25 +0400

    cutter georgian unicode

--- app/models/cutter.rb
+++ app/models/cutter.rb
@@ -8,16 +8,16 @@ class Cutter < ActiveRecord::Base
   	case self.stage
       when 1
         { abonent_number:     self.abonent_number,
-          abonent:            self.abonent, 
+          abonent:            self.abonent.bs_str_to_ka, 
           abonent_type:       self.abonent_type,
-          abonent_address:    self.abonent_address,
+          abonent_address:    self.abonent_address.bs_str_to_ka,
           arrear_date:        self.arrear_date.strftime('%Y-%m-%d'),
           arrear_time:        self.arrear_date.strftime('%H:%M'),
           transaction_number: self.transaction_number }
       when 2
-        { attach_3_2: c.attach_3_2 }
+        { attach_3_2: self.attach_3_2 }
       when 3
-        { attach_3_3: c.attach_3_3 }
+        { attach_3_3: self.attach_3_3 }
     end
   end
 
