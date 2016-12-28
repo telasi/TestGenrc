f21978b381f308bc926a3a1dc677a5ba3d3c3f86
T <opit@gol.ge>
2016-12-28 15:44:36 +0400

    newcust

--- app/models/newcust.rb
+++ app/models/newcust.rb
@@ -12,7 +12,7 @@ class Newcust < ActiveRecord::Base
         fields = [:letter_number, :applicant, :applicant_address, :voltage,
                   :power, :appeal_date, :attach_7_1, :attach_7_1_filename]
       when 2
-        fields = [:attach_7_2, :attach_7_2_filename, :attach_7_4, :attach_7_4_filename]
+        fields = [:attach_7_2, :attach_7_2_filename]
       when 3
         fields = [:transaction_number_3, :the_amount_of_compensation]
     end
