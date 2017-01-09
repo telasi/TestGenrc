6d797a2523d7aa6c0c69b6073a2ce5bc4c062f44
bacho <bachosh2@gmail.com>
2017-01-09 17:25:11 +0400

    gnerc cron

--- app/workers/queue_processor.rb
+++ app/workers/queue_processor.rb
@@ -15,6 +15,40 @@ class QueueProcessor
     end
   end
 
+  def self.send_item_cron(item)
+    clazz = item.service.constantize
+    clazz.connection
+    obj = clazz.find(item.service_id)
+    result = Client.send_b(obj, item.stage)
+    if result && result.key?('result') && result['result'] == "OK"
+      obj.update_stage_date if obj.respond_to?('update_stage_date')
+      obj.update_attributes!(response_id: result['id']) if result['id'].present?
+      obj.update_attributes!(gnerc_status: result['status']) if result['status'].present?
+      ##
+      item2=SendQueue.find_by(id: item.id)
+      item2.update(sent_at: Time.now)
+      item2.save
+      #item.update_attributes(sent_at: Time.now)
+      Log.create!(service: item.service, service_id: item.service_id, action: "send #{item.stage}", success: 1, error: nil)
+    elsif result && result.key?('error')
+      if result.key?('error') && result['error'] == "12"
+        item2=SendQueue.find_by(id: item.id)
+        item2.update(sent_at: Time.now)
+        item2.save
+      else
+        raise Exception.new(result['message'])
+      end
+    else
+      raise Exception.new('Unknown error')
+    end
+  rescue Exception => e
+    Log.create!(service: item.service, service_id: item.service_id, action: "send #{item.stage}", success: 0, error: e.message[0..3999])
+    #raise e
+  end
+
+
+
+
   def send_item(item)
     clazz = item.service.constantize
     clazz.connection
@@ -45,6 +79,9 @@ class QueueProcessor
     Log.create!(service: item.service, service_id: item.service_id, action: "send #{item.stage}", success: 0, error: e.message[0..3999])
     #raise e
   end
+
+
+
 
 end
 
