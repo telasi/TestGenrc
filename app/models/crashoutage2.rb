24a9f41e1542a68410aea58244ce81d5df772a22
bacho <bachosh2@gmail.com>
2017-01-18 14:27:28 +0400

    gnerc outage corr

--- app/models/crashoutage2.rb
+++ app/models/crashoutage2.rb
@@ -24,8 +24,8 @@ class Crashoutage2 < ActiveRecord::Base
           compare_time_2:                            compare_date_2,
           transaction_number_5:                    self.attach_2_1,
           response_id:                             self.attach_2_1,  
-          attach_2_2:                              self.attach_2_1,
-          attach_2_2_filename:                     " ",
+          #attach_2_2:                              self.attach_2_1,
+          #attach_2_2_filename:                     " ",
 
           information_dissemination_actual_date:   self.inf_dissemination_actual_date,
           information_dissemination_actual_time:   self.inf_dissemination_actual_date,
